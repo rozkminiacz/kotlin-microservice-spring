49299f2bd320cd498276dda9884e947cdef9eae5
Rozkmin <jaroslaw.michalik.rozkmin@gmail.com>
2017-08-21 23:55:37 +0200

    Added simple stream

---
+++ src/main/kotlin/com/rozkmin/microservice/StreamController.kt
@@ -0,0 +1,56 @@
+package com.rozkmin.microservice
+
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+
+/**
+ * Created by michalik on 21.08.17
+ */
+@RestController
+@CrossOrigin(origins = arrayOf("http://localhost:8080/"))
+
+@RequestMapping("/stream")
+class StreamController {
+    val emmiters: ArrayList<SseEmitter> = arrayListOf<SseEmitter>()
+
+
+    @GetMapping
+    fun subscribeToStream(): SseEmitter {
+        var element = SseEmitter()
+        emmiters.add(element)
+
+        element.onCompletion({ this.emmiters.remove(element) })
+        element.onTimeout({ this.emmiters.remove(element) })
+
+        return element;
+    }
+
+
+    @PostMapping
+    fun notifyAll(@RequestBody streamData: StreamData): StreamData {
+        println(streamData)
+        emmiters.forEach({
+            try {
+                it.send(streamData)
+            } catch (e: Exception) {
+                println(e)
+                emmiters.remove(it)
+            }
+        })
+        return streamData
+    }
+
+}
+
+class StreamData {
+
+    lateinit var id: String
+    var content: String = ""
+
+    constructor(content: String) {
+        this.content = content
+    }
+
+    constructor()
+
+}
