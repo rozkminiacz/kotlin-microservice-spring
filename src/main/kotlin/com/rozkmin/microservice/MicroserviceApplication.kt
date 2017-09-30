1290a0068d9e5ff1f4a4841ff42efc4e411d91ac
jaroslawmichalik <michalik@protonmail.ch>
2017-09-30 14:52:47 +0200

    Basic implementation of zuul gateway

--- src/main/kotlin/com/rozkmin/microservice/MicroserviceApplication.kt
+++ src/main/kotlin/com/rozkmin/microservice/MicroserviceApplication.kt
@@ -3,10 +3,11 @@ package com.rozkmin.microservice
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
+@EnableZuulProxy
 @SpringBootApplication
 @EnableEncryptableProperties
-
 class MicroserviceApplication
 
 fun main(args: Array<String>) {
