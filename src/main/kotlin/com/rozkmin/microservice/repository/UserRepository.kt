c0c36866c5b917042e6f363ca33b248f9af650ef
jaroslawmichalik <michalik@protonmail.ch>
2017-10-08 17:40:55 +0200

    Added mysql with sample config

--- src/main/kotlin/com/rozkmin/microservice/repository/UserRepository.kt
+++ src/main/kotlin/com/rozkmin/microservice/repository/UserRepository.kt
@@ -1,11 +1,13 @@
 package com.rozkmin.microservice.repository
 
 import com.rozkmin.microservice.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : CrudRepository<User, String> {
+interface UserRepository : JpaRepository<User, String> {
     fun findOneByUsername(username: String): User
     fun findByUsername(name: String): MutableIterable<User>
     fun findByUsernameContaining(phrase: String): MutableIterable<User>
